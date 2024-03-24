2024c8d17f3ce9e256a837811cd881a1f29928bc
hellp31 <144005504+hellp31@users.noreply.github.com>
2024-03-24 14:25:10 +0300

    Add files via upload

---
+++ kursovay/helpers/function.php
@@ -0,0 +1,23 @@
+<?php
+/* сервер идентифицирует браузер */
+session_start();
+//echo phpinfo();
+
+$db = mysqli_connect("localhost", "root", "", "test2");
+if (!$db) {
+    die('Ошибка подключения');
+}
+
+function login($name, $psw, $family)
+{
+    global $db;
+    $result = mysqli_query($db, "SELECT * FROM `users` WHERE name='$name' AND psw=md5('$psw') AND family='$family';");
+    return mysqli_num_rows($result);
+}
+function student()
+{
+    global $db;
+    $result = mysqli_query($db, "SELECT  *  FROM `users`  ;" );
+    return mysqli_fetch_all($result, MYSQLI_ASSOC );
+
+}
