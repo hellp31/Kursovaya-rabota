2e3c6309271668462a906f864f0d33164cdec1f7
hellp31 <144005504+hellp31@users.noreply.github.com>
2024-04-11 09:39:49 +0300

    Add files via upload

--- kursovay/scripts/main.js
+++ kursovay/scripts/main.js
@@ -1,41 +1,52 @@
-var images = [
-    '../images/Линии.png',
-    '../images/песик.jpg',
-    '../images/Человечек.png',
-    '../images/logo.jpg'
-  ];
-  var currentImageIndex = 0;
-  
-  document.addEventListener('DOMContentLoaded', function() {
-    var slider = document.querySelector('.slider');
-    var image = document.createElement('img');
-    image.src = images[currentImageIndex];
-    
-  
-    var previousButton = document.createElement('button');
-    previousButton.textContent = '←--';
-    previousButton.style.marginTop = '20px'
-    previousButton.addEventListener('click', function() {
-      currentImageIndex = (currentImageIndex + images.length - 1) % images.length;
-      image.src = images[currentImageIndex];
-    });
-    slider.appendChild(previousButton);
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
 
-      var image = document.createElement('img');
-    image.src = images[currentImageIndex];
-    image.style.width = '500px';
-    image.style.height = '500px';
-    image.style.marginTop = '-150px'
-    slider.appendChild(image);
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
 
-    var nextButton = document.createElement('button');
-    nextButton.textContent = '--→';
-    nextButton.style.marginTop = '20px'
-    nextButton.addEventListener('click', function() {
-      currentImageIndex = (currentImageIndex + 1) % images.length;
-      image.src = images[currentImageIndex];
-    });
-    slider.appendChild(nextButton);
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
 
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
 
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
   });
+}
+
+// Инициализация слайдера
+updateSlider();
+
+
+
+function replaceButton() {
+  var button1 = document.getElementById('button1');
+  var button2 = document.getElementById('button2');
+  
+  if (button1.innerHTML === 'Развернуть') {
+button2.parentNode.replaceChild(button1, button2);
+      
+  } elif(button2.innerHTML === 'Cвернуть') ;{
+      
+      button1.parentNode.replaceChild(button2, button1);
+  }
+}
